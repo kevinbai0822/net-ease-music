c6e6247626c081720072132f818a753e776e968a
jarvis <735731342@qq.com>
2017-09-13 20:46:05 +0800

    vue自建项目

---
+++ src/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        勇敢的少年啊快去创造奇迹
+    </div>
+</template>
+<script>
+export default {
+    name: 'index',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style>
+
+</style>
+
